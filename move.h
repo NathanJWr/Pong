9b12ad4b2fd6ae22534c173fe7c9ddd628e3c943
NathanJWr <nathanwright1623@gmail.com>
2018-05-09 20:44:46 -0700

    Moved "move" funtions into their own file

---
+++ move.h
@@ -0,0 +1,8 @@
+#ifndef MOVE_H
+#define MOVE_H
+#include <SDL2/SDL.h>
+#include "structs.h"
+void moveAIPaddle(SDL_Rect* paddle, Ball* ball);
+void movePaddle(SDL_Event* e, SDL_Rect* paddle);
+void moveBall(Ball* ball, SDL_Rect* paddle1);
+#endif
